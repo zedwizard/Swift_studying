0b201baddad331156a73572451c506e8ac3a8493
zed <jahkushkin@gmail.com>
2019-09-05 16:57:22 +0300

    test commit

---
+++ HW2.3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW2.3
+//
+//  Created by Zed on 9/3/19.
+//  Copyright © 2019 123. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
